ef77c62fbfee946578d3451b7dfa972e67503f84
“00010590wiut” <00010590wiut@gmail.com>
2022-11-08 23:24:53 +0500

    MVC APP commit

---
+++ ASP.NET MVC/Controllers/AnimalController.cs
@@ -0,0 +1,125 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_MVC.Models;
+
+namespace ASP.NET_MVC.Controllers
+{
+    public class AnimalController : Controller
+    {
+
+        // async Because we are connecting to API
+        // Task<ActionsResults>
+
+        // GET: Product
+        public async Task<ActionResult> Index()
+        {
+            //Hosted web API REST Service base url
+            string Baseurl = "https://localhost:7147/";
+            List<Animal> Animals = new List<Animal>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new
+                MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource using HttpClient
+                HttpResponseMessage Res = await client.GetAsync("api/Animal");
+
+                //Checking the response is successful or not which is sent HttpClient
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api
+                    var AnimalResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing the Product list
+                    Animals = JsonConvert.DeserializeObject<List<Animal>>(AnimalResponse);
+                }
+                //returning the Product list to view
+                return View(Animals);
+            }
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ASP.NET MVC/Models/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public AnimalSpecies AnimalSpecies { get; set; }
+    }
+}
